f0e79fe3b42e13c02f828d84e2751a6e6271f8fa
Tannuk17 <144197204+Tannuk17@users.noreply.github.com>
2023-09-06 09:32:11 +0530

    added python file

---
+++ main.py
@@ -0,0 +1,11 @@
+def to_upper(name):
+    return name.upper()
+
+def say_hello(name):
+    print(f'Hello,{name}')
+
+    if _name_ == '_main_':
+        name='TrainwithTannu'
+        say_hello(name)
+        up=to_upper(name)
+        print(up)
